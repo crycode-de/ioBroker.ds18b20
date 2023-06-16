e8daf700d653cefd7e18a5a4f0ea6cf23fbbaf3f
Peter Müller <peter@crycode.de>
2023-06-16 17:27:41 +0200

    Fixed remote client setup build

--- build-remote-client-setup.js
+++ build-remote-client-setup.js
@@ -8,6 +8,6 @@ const files = fs.readdirSync(path.join('build', 'remote'))
   .filter((f) => f.endsWith('.js') && f !== 'setup.js');
 
 const setup = fs.readFileSync(path.join('build', 'remote', 'setup.js'), 'utf-8')
-  .replace('FILE: \'DUMMY\'', files.map((f) => `'${f}': '${Buffer.from(fs.readFileSync(path.join('build', 'remote', f), 'utf-8')).toString('base64')}'`).join(',\n'));
+  .replace(/FILE: ['"]DUMMY['"]/, files.map((f) => `'${f}': '${Buffer.from(fs.readFileSync(path.join('build', 'remote', f), 'utf-8')).toString('base64')}'`).join(',\n'));
 
 fs.writeFileSync(path.join('admin','remote-client-setup.js'), setup, 'utf-8');
