376cb8cd1e3242d0ffa649405a10782c286bd4a9
Peter Müller <peter@crycode.de>
2023-06-16 17:27:06 +0200

    Back to aes-256-cbc for now

--- src/remote/common.ts
+++ src/remote/common.ts
@@ -24,7 +24,7 @@ const IV_LENGTH = 16;
  */
 export function encrypt (text: crypto.BinaryLike, key: Buffer): string {
   const iv = crypto.randomBytes(IV_LENGTH);
-  const cipher = crypto.createCipheriv('aes-256-gcm', key, iv);
+  const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
   let encrypted = cipher.update(text);
 
   encrypted = Buffer.concat([ encrypted, cipher.final() ]);
@@ -42,7 +42,7 @@ export function decrypt (text: string, key: Buffer): string {
   const textParts = text.split(':');
   const iv = Buffer.from(textParts.shift() as string, 'hex');
   const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv);
+  const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
   let decrypted = decipher.update(encryptedText);
 
   decrypted = Buffer.concat([ decrypted, decipher.final() ]);
